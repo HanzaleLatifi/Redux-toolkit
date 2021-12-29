a97e86d7233bdcff5c7a69235f64bf24e0f5bd45
h8 <h.latifi03@gmail.com>
2021-12-29 21:05:49 +0330

    use global state in compnent by to props

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,24 @@
 import React from 'react'
-
-function CakeContainer() {
+import {connect} from "react-redux"
+import { buyCake } from '../redux/cake/cakeActions'
+function CakeContainer(props) {
     return (
         <div>
-            <p>number o cakes</p>
-            <button>buyCake</button>
+            <p>number of cakes {props.num_Of_Cakes}</p>
+            <button onClick={props.buyCake}>buyCake</button>
         </div>
     )
 }
 
-export default CakeContainer
+const mapStateToProps=(state)=>{
+    return{
+        num_Of_Cakes:state.numOfCakes
+    }
+}
+const mapDispatchToProps=(dispath)=>{
+    return{
+        buyCake:()=> dispath(buyCake())  // buycake : ()=> dispath ({ type:  ,  })
+    }
+}
+
+export default connect(mapStateToProps , mapDispatchToProps)(CakeContainer)
