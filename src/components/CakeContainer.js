3f7184cbb105ca6294acd0f4ded64a7f13c24266
h8 <h.latifi03@gmail.com>
2021-12-29 21:14:35 +0330

    use global state with useSelector & useDispathc

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,24 +1,29 @@
 import React from 'react'
-import {connect} from "react-redux"
+import {connect , useSelector , useDispatch} from "react-redux"
 import { buyCake } from '../redux/cake/cakeActions'
+
 function CakeContainer(props) {
+    const numOfCakes=useSelector((state)=>state.numOfCakes)
+    const dispatch=useDispatch()
     return (
         <div>
-            <p>number of cakes {props.num_Of_Cakes}</p>
-            <button onClick={props.buyCake}>buyCake</button>
+            {/* <p>number of cakes {props.num_Of_Cakes}</p>
+            <button onClick={props.buyCake}>buyCake</button> */}
+            <p>number of cakes {numOfCakes}</p>
+            <button onClick={()=>dispatch(buyCake())}>buyCake</button>
         </div>
     )
 }
+export default CakeContainer
+// const mapStateToProps=(state)=>{
+//     return{
+//         num_Of_Cakes:state.numOfCakes
+//     }
+// }
+// const mapDispatchToProps=(dispath)=>{
+//     return{
+//         buyCake:()=> dispath(buyCake())  // buycake : ()=> dispath ({ type:  ,  })
+//     }
+// }
 
-const mapStateToProps=(state)=>{
-    return{
-        num_Of_Cakes:state.numOfCakes
-    }
-}
-const mapDispatchToProps=(dispath)=>{
-    return{
-        buyCake:()=> dispath(buyCake())  // buycake : ()=> dispath ({ type:  ,  })
-    }
-}
-
-export default connect(mapStateToProps , mapDispatchToProps)(CakeContainer)
+// export default connect(mapStateToProps , mapDispatchToProps)(CakeContainer)
