88b5c1d28f2d534a7bd0e831b042ab35fbbbba13
h8 <h.latifi03@gmail.com>
2022-01-02 23:00:06 +0330

    add Todos skelet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./feature/store";
 import Counter from "./components/Counter";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteItems";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Provider store={store}>
-      <Counter />
+      {/* <Counter /> */}
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
     </Provider>
   );
 }
